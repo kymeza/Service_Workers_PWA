418b6442830598052d3598afdbc553dbe4245afc
Kyron Meza <kyron.meza@outlook.com>
2024-01-02 22:33:07 -0300

    Code cleanup

--- my-service-worker-app/public/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('push', event => {
-    const data = event.data.json();
-    console.log('Push received...');
-    self.registration.showNotification(data.title, {
-      body: 'Notified by Service Worker',
-      icon: 'icon.png'
-    });
-  });
